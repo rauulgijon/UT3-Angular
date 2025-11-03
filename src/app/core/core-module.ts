9543497d834ecf43306a71132cc8fa44bdba4169
rauulgijon <raulgijon16@gmail.com>
2025-11-03 09:32:55 +0100

    core y shared

---
+++ src/app/core/core-module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+// Definir servicios, modelos, dependencias centrales...
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
